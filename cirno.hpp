7a0e7926c8d020e0de5770b26b69930e21291af6
lolisa <lolisa@marisa.moe>
2015-09-20 14:27:57 -0700

    return of the expression

--- cirno.hpp
+++ cirno.hpp
@@ -4,14 +4,16 @@
 #include <iostream>
 #include <../algebraic_data_type/algebraic_data_type.hpp>
 using namespace algebraic_data_type;
-typedef algebraic_data_type< std::string, std::tuple< recursive_indicator, recursive_indicator > > expr;
+typedef algebraic_data_type< std::string, std::tuple< recursive_indicator, recursive_indicator >, unit > expr;
 DECLARE_CONSTRUCTOR( expr, 0, print, x );
 DECLARE_CONSTRUCTOR( expr, 1, seq, x );
-void execute(const expr & s)
+DECLARE_CONSTRUCTOR( expr, 2, UNIT, x );
+expr execute(const expr & s)
 {
-    s.match< print< arg >, seq< arg, arg > >( common::make_expansion(
-        []( const std::string & str ){ std::cout << str; },
-        []( const expr & l, const expr & r ){ execute( l ); execute( r ); } ) );
+    return s.match< print< arg >, seq< arg, arg >, UNIT< > >( common::make_expansion(
+        []( const std::string & str ){ std::cout << str; return expr( UNIT< >( ) ); },
+        common::make_expansion( []( const expr & l, const expr & r ){ execute( l ); return execute( r ); },
+        []( ){ return expr( UNIT< >( ) ); } ) ) );
 }
 #endif // CIRNO_HPP
 
