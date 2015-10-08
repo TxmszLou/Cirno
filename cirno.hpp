475b6840101987d3391af4db36f8545f989c023f
Heather <Heather@live.ru>
2015-10-08 13:18:49 +0400

    add when, unless

--- cirno.hpp
+++ cirno.hpp
@@ -7,15 +7,17 @@ using namespace algebraic_data_type;
 typedef
 algebraic_data_type
 <
-    recursive_indicator,
-    std::tuple< recursive_indicator, recursive_indicator >,
-    unit,
-    unit,
-    unit,
-    std::tuple< recursive_indicator, recursive_indicator, recursive_indicator >,
-    std::string
+    recursive_indicator, // Print
+    std::tuple< recursive_indicator, recursive_indicator >, // Seq
+    unit, // Unit
+    unit, // True
+    unit, // False
+    std::tuple< recursive_indicator, recursive_indicator >, // When
+    std::tuple< recursive_indicator, recursive_indicator >, // Unless
+    std::tuple< recursive_indicator, recursive_indicator, recursive_indicator >, // If
+    std::string // String
 > expr;
-DECLARE_CONSTRUCTORS( expr, (Print, Seq, Unit, True, False, If, String), x )
+DECLARE_CONSTRUCTORS( expr, ( Print, Seq, Unit, True, False, When, Unless, If, String), x )
 
 bool value_to_bool( const expr & e )
 { return e.match( with( True( uim ), []( ){ return true; } ), with( False( uim ), []( ){ return false; } ) ); }
@@ -38,6 +40,14 @@ expr execute( const expr & e )
             with( Unit( uim ), []( ){ return Unit( ); } ),
             with( True( uim ), []( ){ return True( ); } ),
             with( False( uim ), []( ){ return False( ); } ),
+            with(
+                When( arg, arg ),
+                []( const expr & i, const expr & exp )
+                { return value_to_bool( execute( i ) ) ? execute( exp ) : execute( exp ); } ),
+            with(
+                Unless( arg, arg ),
+                []( const expr & i, const expr & exp )
+                { return value_to_bool( execute( i ) ) ? Unit( ) : execute( exp ); } ),
             with(
                 If( arg, arg, arg ),
                 []( const expr & i, const expr & t, const expr & e )
