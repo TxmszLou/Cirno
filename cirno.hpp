43f540fed4ac62db1b607a9533af99134a8fd653
lolisa <lolisa@marisa.moe>
2015-10-10 00:52:59 -0700

    adding variable and assignment

--- cirno.hpp
+++ cirno.hpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>
 #include <../algebraic_data_type/algebraic_data_type.hpp>
+#include <map>
+#include <utility>
 using namespace algebraic_data_type;
 typedef recursive_indicator ri;
 
@@ -15,8 +17,13 @@ DECLARE_ADT( expr,
            (If,         ri, ri, ri),
            (String,     std::string),
            (Show,       ri),
-           (Concat,     ri, ri)
+           (Concat,     ri, ri),
+           (Set,        ri, ri),
+           (Get,        ri),
+           (IsDefined,  ri)
          ), X )
+
+typedef std::map< std::string, expr > symbol_table;
 
 expr When( const expr & con, const expr & act ) { return If( con, act, Unit( ) ); }
 expr Unless( const expr & con, const expr & act ) { return If( con, Unit( ), act ); }
@@ -38,24 +45,40 @@ std::string show( const expr & e )
                 with( String( arg ), []( const std::string & str ) { return str; } ) );
 }
 
-expr execute( const expr & e )
+expr execute( symbol_table & st, const expr & e )
 {
     return
         e.match(
-            with( Print( arg ), []( const expr & exp ) { std::cout << show( execute( exp ) ); return Unit( ); } ),
-            with( Seq( arg, arg ), []( const expr & l, const expr & r ) { execute( l ); return execute( r ); } ),
+            with( Print( arg ), [&]( const expr & exp ) { std::cout << show( execute( st, exp ) ); return Unit( ); } ),
+            with( Seq( arg, arg ), [&]( const expr & l, const expr & r ) { execute( st, l ); return execute( st ,r ); } ),
             with( Unit( uim ), []( ){ return Unit( ); } ),
             with( True( uim ), []( ){ return True( ); } ),
             with( False( uim ), []( ){ return False( ); } ),
             with(
                 If( arg, arg, arg ),
-                []( const expr & i, const expr & t, const expr & e )
-                { return value_to_bool( execute( i ) ) ? execute( t ) : execute( e ); } ),
+                [&]( const expr & i, const expr & t, const expr & e )
+                { return value_to_bool( execute( st, i ) ) ? execute( st, t ) : execute( st, e ); } ),
             with( String( arg ), []( const std::string & str ) { return String( str ); } ),
-            with( Show( arg ), []( const expr & e ) { return String( show( execute( e ) ) ); } ),
+            with( Show( arg ), [&]( const expr & e ) { return String( show( execute( st, e ) ) ); } ),
             with(
                 Concat( arg, arg ),
-                []( const expr & l, const expr & r ) { return String( value_to_string( execute( l ) ) + value_to_string( execute( r ) ) ); } ) );
+                [&]( const expr & l, const expr & r )
+                { return String( value_to_string( execute( st, l ) ) + value_to_string( execute( st, r ) ) ); } ),
+            with(
+                IsDefined( arg ),
+                [&]( const expr & exp ) { return st.count( value_to_string( execute( st, exp ) ) ) != 0 ? True( ) : False( ); } ),
+            with( Get( arg ), [&]( const expr & e ) { return st.find( value_to_string( execute( st, e ) ) )->second; } ),
+            with(
+                Set( arg, arg ),
+                [&]( const expr & l, const expr & r )
+                {
+                    auto it = st.insert( std::make_pair( value_to_string( execute( st, l ) ), r ) );
+                    if ( ! it.second ) { it.first->second = r; }
+                    return Unit( );
+                } ) );
 }
-#endif // CIRNO_HPP
 
+expr execute( std::tuple< symbol_table > st, const expr & e ) { return execute( std::get< 0 >( st ), e ); }
+expr execute( const expr & e ) { return execute( std::make_tuple( symbol_table( ) ), e ); }
+
+#endif // CIRNO_HPP
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cirno.hpp>
+#include <cassert>
 int main( )
 {
     execute( Seq(
@@ -9,4 +10,10 @@ int main( )
         Unless( False( ), Print( String( "world!\n" ) ) ) ) );
     execute( Print( Print( Seq( Print( True( ) ), Print( False( ) ) ) ) ) );
     execute( Print( Concat( String( "4" ), Show( True( ) ) ) ) );
+    assert( value_to_bool( execute( IsDefined( String( "not defined" ) ) ) ) == false );
+    execute(
+        Seq( Set( String( "SomeVar" ), String( "12" ) ),
+             Seq( Print( Get( String( "SomeVar" ) ) ),
+                  Seq( Set( Concat( String( "S" ), String( "omeVar" ) ), String( "345" ) ),
+                       Print( Get( Concat( String( "Some" ), String( "Var" ) ) ) ) ) ) ) );
 }
