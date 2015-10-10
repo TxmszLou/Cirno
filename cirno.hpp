c980663b6ef45a8c1d8a5341c7e007d2df5c7dcf
lolisa <lolisa@marisa.moe>
2015-10-10 16:59:53 -0700

    fix #15, add define

--- cirno.hpp
+++ cirno.hpp
@@ -5,6 +5,7 @@
 #include <../algebraic_data_type/algebraic_data_type.hpp>
 #include <map>
 #include <utility>
+#include <experimental/optional>
 using namespace algebraic_data_type;
 typedef recursive_indicator ri;
 
@@ -21,10 +22,34 @@ DECLARE_ADT( expr,
            (Set,        ri, ri),
            (Get,        ri),
            (IsDefined,  ri),
+           (Define,     ri, ri),
            (Scope,      ri)
          ), X )
 
 typedef std::map< std::string, expr > symbol_table;
+
+struct environment
+{
+    symbol_table st;
+    std::experimental::optional< std::reference_wrapper< environment > > parent;
+    typedef std::experimental::optional< expr > value;
+    value get( const std::string & str )
+    {
+        auto it = st.find( str );
+        return it != st.end( ) ? it->second : ( parent ? parent->get( ).get( str ) : value{ } );
+    }
+    void define( const std::string & str, const expr & e )
+    {
+        auto res = st.insert( std::make_pair( str ,e ) );
+        assert( res.second );
+    }
+    bool assign( const std::string & str, const expr & e )
+    {
+        auto it = st.find( str );
+        return it != st.end( ) ? ( it->second = e, true ) : ( parent ? parent->get( ).assign( str, e ) : false );
+    }
+    void set( const std::string & str, const expr & e ) { if ( ! assign( str, e ) ) { st.insert( std::make_pair( str, e ) ); } }
+};
 
 expr When( const expr & con, const expr & act ) { return If( con, act, Unit( ) ); }
 expr Unless( const expr & con, const expr & act ) { return If( con, Unit( ), act ); }
@@ -46,42 +71,44 @@ std::string show( const expr & e )
                 with( String( arg ), []( const std::string & str ) { return str; } ) );
 }
 
-expr execute( std::tuple< symbol_table > st, const expr & e );
-expr execute( symbol_table & st, const expr & e )
+expr execute( std::tuple< environment > st, const expr & e );
+expr execute( environment & env, const expr & e )
 {
     return
         e.match(
-            with( Print( arg ), [&]( const expr & exp ) { std::cout << show( execute( st, exp ) ); return Unit( ); } ),
-            with( Seq( arg, arg ), [&]( const expr & l, const expr & r ) { execute( st, l ); return execute( st ,r ); } ),
+            with( Print( arg ), [&]( const expr & exp ) { std::cout << show( execute( env, exp ) ); return Unit( ); } ),
+            with( Seq( arg, arg ), [&]( const expr & l, const expr & r ) { execute( env, l ); return execute( env,r ); } ),
             with( Unit( uim ), []( ){ return Unit( ); } ),
             with( True( uim ), []( ){ return True( ); } ),
             with( False( uim ), []( ){ return False( ); } ),
             with(
                 If( arg, arg, arg ),
                 [&]( const expr & i, const expr & t, const expr & e )
-                { return value_to_bool( execute( st, i ) ) ? execute( st, t ) : execute( st, e ); } ),
+                { return value_to_bool( execute( env, i ) ) ? execute( env, t ) : execute( env, e ); } ),
             with( String( arg ), []( const std::string & str ) { return String( str ); } ),
-            with( Show( arg ), [&]( const expr & e ) { return String( show( execute( st, e ) ) ); } ),
+            with( Show( arg ), [&]( const expr & e ) { return String( show( execute( env, e ) ) ); } ),
             with(
                 Concat( arg, arg ),
                 [&]( const expr & l, const expr & r )
-                { return String( value_to_string( execute( st, l ) ) + value_to_string( execute( st, r ) ) ); } ),
+                { return String( value_to_string( execute( env, l ) ) + value_to_string( execute( env, r ) ) ); } ),
             with(
                 IsDefined( arg ),
-                [&]( const expr & exp ) { return st.count( value_to_string( execute( st, exp ) ) ) != 0 ? True( ) : False( ); } ),
-            with( Get( arg ), [&]( const expr & e ) { return st.find( value_to_string( execute( st, e ) ) )->second; } ),
+                [&]( const expr & exp ) { return env.get( value_to_string( execute( env, exp ) ) ) ? True( ) : False( ); } ),
+            with(
+                Define( arg, arg ),
+                [&]( const expr & l, const expr & r ) { env.define( value_to_string( execute( env, l ) ), r ); return Unit( ); } ),
+            with( Get( arg ), [&]( const expr & e ) { return *env.get( value_to_string( execute( env, e ) ) ); } ),
             with(
                 Set( arg, arg ),
                 [&]( const expr & l, const expr & r )
                 {
-                    auto it = st.insert( std::make_pair( value_to_string( execute( st, l ) ), r ) );
-                    if ( ! it.second ) { it.first->second = r; }
+                    env.set( value_to_string( execute( env, l ) ), r );
                     return Unit( );
                 } ),
-            with( Scope( arg ), [&]( const expr & e ) { return execute( std::make_tuple( symbol_table( st ) ), e ); } ) );
+            with( Scope( arg ), [&]( const expr & e ) { return execute( std::make_tuple( environment{ symbol_table{ }, { env } } ), e ); } ) );
 }
 
-expr execute( std::tuple< symbol_table > st, const expr & e ) { return execute( std::get< 0 >( st ), e ); }
-expr execute( const expr & e ) { return execute( std::make_tuple( symbol_table( ) ), e ); }
+expr execute(std::tuple< environment > st, const expr & e ) { return execute( std::get< 0 >( st ), e ); }
+expr execute( const expr & e ) { return execute( std::make_tuple( environment{ } ), e ); }
 
 #endif // CIRNO_HPP
