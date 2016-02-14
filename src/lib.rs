d33f6dace2cf3d345f86e77057f15edf12583243
Marisa Kirisame <lolisa@marisa.moe>
2016-02-14 07:40:40 -0800

    rewrite with rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+#![feature(box_syntax, box_patterns)]
+Enum Term {
+    TString(String),
+    TBool(bool),
+    TCon(Box<Term>, Box<Term>, Box<Term>)
+}
+use Term::*;
+fn eval(t : Term) -> Term {
+    match t {
+        TString(_) => t,
+        TBool(_) => t,
+        TCon(box i, box t, box e) =>
+            match eval(i) {
+                TBool(true) => eval(t),
+                TBool(false) => eval(e),
+                _ => unreachable!()
+            }
+    }
+}
 #[test]
 fn it_works() {
 }
