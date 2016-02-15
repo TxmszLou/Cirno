1b0394a0b6da59232331e2fb7f0cd37b6c3d4d98
Marisa Kirisame <lolisa@marisa.moe>
2016-02-15 06:32:16 -0800

    add more code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,39 @@
 #![feature(box_syntax, box_patterns)]
-Enum Term {
+#[derive(PartialEq, PartialOrd, Debug)]
+enum Term
+{
     TString(String),
     TBool(bool),
-    TCon(Box<Term>, Box<Term>, Box<Term>)
+    TCon(Box<Term>, Box<Term>, Box<Term>),
+    TPrintLn(Box<Term>),
+    TUnit
 }
 use Term::*;
-fn eval(t : Term) -> Term {
-    match t {
+fn eval(t : Term) -> Term
+{
+    match t
+    {
         TString(_) => t,
         TBool(_) => t,
+        TUnit => t,
         TCon(box i, box t, box e) =>
             match eval(i) {
                 TBool(true) => eval(t),
                 TBool(false) => eval(e),
                 _ => unreachable!()
+            },
+        TPrintLn(box s) =>
+            match eval(s) {
+                TString(x) => { println!("{}", x); TUnit },
+                _ => unreachable!()
             }
     }
 }
 #[test]
-fn it_works() {
+fn test_conditional(){
+    assert_eq!(eval(TCon(
+        box TBool(true),
+        box TString(String::from("Hello")),
+        box TString(String::from("World")))),
+               TString(String::from("Hello")))
 }
